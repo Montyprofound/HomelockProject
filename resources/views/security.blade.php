2cc18611b9a64f6fb4ba5e6f835eba9e57280918
Montyprofound <mosesmwendwamwema@gmail.com>
2025-11-25 15:15:58 +0300

    Added error handling display

--- resources/views/security.blade.php
+++ resources/views/security.blade.php
@@ -132,6 +132,18 @@
                         </div>
                     @endif
                     
+                    @if($errors->any())
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <i class="fas fa-exclamation-triangle me-2"></i>
+                            <ul class="mb-0">
+                                @foreach($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                        </div>
+                    @endif
+                    
                     <form action="{{ route('submit-request') }}" method="POST" id="quoteForm">
                         @csrf
                         <input type="hidden" name="service_type" value="security">
